0ff913bdca20fdcb027eb99bad096f540b715e7e
Danilson Farisato <danilsonfarisato@MacBook-Pro.local>
2020-07-07 14:45:43 +0100

    sixth commit

---
+++ isometrics/isometrics/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ClockComponent } from './components/clock/clock.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    ClockComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
