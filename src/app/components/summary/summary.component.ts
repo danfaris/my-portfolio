4f5e86be4bc19a5d36eb4c7bdb5ef1a72f546fca
Danilson Farisato <danilsonfarisato@MacBook-Pro.local>
2020-10-06 22:00:27 +0100

    final tuesday commit

--- src/app/components/summary/summary.component.ts
+++ src/app/components/summary/summary.component.ts
@@ -11,15 +11,15 @@ export class SummaryComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    const optionsTwo = {
-      strings: ['Bilingual (Portuguese and English) professional with a BSc in Advertising and graduated with a higher diploma in Computer Science at NCI with overall 15 years of experience in entrepreneurship, business management, audiovisual editing/post-production, and 1 year experience with Java programming.'],
-      typeSpeed: 0,
-      backSpeed: 0,
-      showCursor: false,
-      cursorChar: '_',
-      loop: false
-    };
-    const typedTwo = new Typed('.typed-element-two', optionsTwo);
+    // const optionsTwo = {
+    //   strings: ['Bilingual (Portuguese and English) professional with a BSc in Advertising and graduated with a higher diploma in Computer Science at NCI with overall 15 years of experience in entrepreneurship, business management, audiovisual editing/post-production, and 1 year experience with Java programming.'],
+    //   typeSpeed: 0,
+    //   backSpeed: 0,
+    //   showCursor: false,
+    //   cursorChar: '_',
+    //   loop: false
+    // };
+    // const typedTwo = new Typed('.typed-element-two', optionsTwo);
 
   }
 
